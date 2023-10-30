8a4329a00a737a9df11a1e9e600a65c79156a60d
Yasniel <yasnielfajardoegues1408@gmail.com>
2023-10-30 19:40:35 -0300

    test

--- main.go
+++ main.go
@@ -3,5 +3,34 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Init World")
+
+	// String
+	var wathToSay string
+
+	wathToSay = "Godbye cruel world"
+
+	fmt.Println(wathToSay)
+
+	// Integer
+	var number int
+	number = 42
+	fmt.Println("El numero es: ", number)
+
+	//function
+	something := saySomething()
+	fmt.Println("The function return something: ", something)
+
+	something1, something2 := saySomething2()
+	fmt.Println("The function return something: ", something1, something2)
+}
+
+func saySomething() string {
+
+	return "Something"
+}
+
+func saySomething2() (string, string) {
+
+	return "I Say", "Something"
 }
