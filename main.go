f6390beb0a3f1f97148231304631d8645104ed34
Yasniel <yasnielfajardoegues1408@gmail.com>
2023-10-30 22:36:11 -0300

    pointer

--- main.go
+++ main.go
@@ -23,14 +23,27 @@ func main() {
 
 	something1, something2 := saySomething2()
 	fmt.Println("The function return something: ", something1, something2)
+
+	//Pointer
+	color := "Green"
+	fmt.Println("BEFORE: The color is: ", color)
+	changeUsePointer(&color)
+	fmt.Println("AFTER: The color is: ", color)
+
 }
 
 func saySomething() string {
-
 	return "Something"
 }
 
 func saySomething2() (string, string) {
-
 	return "I Say", "Something"
+}
+
+func changeUsePointer(e *string) {
+	fmt.Println("The pointer is: ", e) // The pointer is:  0xc000010200
+	fmt.Println("The value is: ", *e)  // The pointer is:  Green
+
+	newValue := "Red"
+	*e = newValue
 }
